4134fb2e78dfbb1fd572d9f6de662c20eb73870d
Yuya.Nishida <yuya@j96.org>
2023-06-11 13:37:31 +0900

    git dust commit [ci skip]

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,9 @@ set -eux
 
 bundle
 bin/rails \
-  generate_secret_token &&
-  db:create &&
-  db:migrate &&
+  generate_secret_token \
+  db:create \
+  db:migrate \
   db:migrate:status
 
 exec "$@"
